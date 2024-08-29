ac6541a19452e4854dce6fa06ca3b1ccd8f718ee
Farha-rashid-10 <farkaf07@gmail.com>
2024-08-29 23:34:14 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+('Yogi', 5, 'M', 'brown', 'friendly', TRUE),
+('Boo-Boo', 3, 'M', 'brown', 'shy', TRUE),
+('Grizzly', 12, 'M', 'black', 'aggressive', TRUE),
+('Baloo', 10, 'M', 'gray', 'laid-back', TRUE),
+('Paddington', 7, 'M', 'brown', 'curious', TRUE),
+('Teddy', 2, 'M', 'black', 'playful', TRUE),
+('Winnie', 4, 'M', 'yellow', 'sweet', TRUE),
+('unnamed', 8, 'F', 'white', 'calm', TRUE);  -- This is the unnamed bear
